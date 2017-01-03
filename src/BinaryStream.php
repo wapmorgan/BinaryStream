e52645d961d5144726dc5c0297dc3ddabc940feb
wapmorgan <wapmorgan@gmail.com>
2017-01-04 02:54:48 +0300

    Add source

---
+++ src/BinaryStream.php
@@ -0,0 +1,286 @@
+<?php
+namespace wapmorgan\BinaryStream;
+
+class BinaryStream {
+    const BIG = 'big';
+    const LITTLE = 'little';
+
+    protected $fp;
+    protected $offset = 0;
+    protected $bitOffset = 0;
+    protected $cache = array();
+    protected $endian = 'little';
+    protected $types = array(
+        'little' => array(
+            'short' => 'v'
+            'integer' => 'V',
+            'long' => 'P'
+            'float' => 'f',
+            'double' => 'd',
+        ),
+        'big' => array(
+            'short' => 'n'
+            'integer' => 'N',
+            'long' => 'J'
+            'float' => 'f',
+            'double' => 'd',
+        ),
+    );
+    protected $labels = array(
+        'integer' => array(
+            16 => 'short',
+            32 => 'integer',
+            64 => 'long',
+        ),
+        'float' => array(
+            32 => 'float',
+            64 => 'double',
+        ),
+    );
+    protected $groups = array();
+    protected $marks = array();
+
+    public function __construct($file) {
+        if (is_string($file)) {
+            if (!file_exists($file))
+                $this->fp = fopen($file, 'w');
+            else
+                $this->fp = fopen($file, 'r');
+        } else if (is_resource($file)) {
+            $this->fp = $file;
+        }
+    }
+
+    public function readBit() {
+        if ($this->bitOffset == 8) {
+            $this->bitOffset = 0;
+            $this->offset++;
+        }
+
+        if (!isset($cache[$this->offset]))
+            $cache[$this->offset] = $this->readChar();
+
+        $this->bitOffset++;
+        return (bool) ($cache[$this->offset] >> $this->bitOffset);
+    }
+
+    public function readBits(array $bits) {
+        if ($this->bitOffset == 8) {
+            $this->bitOffset = 0;
+            $this->offset++;
+        }
+
+        if (!isset($cache[$this->offset]))
+            $cache[$this->offset] = $this->readChar();
+
+        $result = array();
+        foreach ($bits as $bitName) {
+            $this->bitOffset++;
+            $result[$bitName] = (bool) ($cache[$this->offset] >> $this->bitOffset);
+        }
+        return $result;
+    }
+
+    public function readInteger($sizeInBits = 32) {
+        if ($sizeInBits >= 16 && $sizeInBits <= 64 && $sizeInBits % 8 == 0) {
+            $bytes = $sizeInBits / 8;
+            $data = fread($this->fp, $bytes);
+            if ($data !== false)
+                $this->offset += $bytes;
+            else
+                $this->offset = ftell($this->fp);
+
+            $value = unpack($this->types[$this->endian][$this->labels['integer'][$sizeInBits]], $data);
+            return $value;
+        }
+    }
+
+    public function readFloat($sizeInBits = 32) {
+        if ($sizeInBits == 32 || $sizeInBits == 64) {
+            $bytes = $sizeInBits / 8;
+            $data = fread($this->fp, $bytes);
+            if ($data !== false)
+                $this->offset += $bytes;
+            else
+                $this->offset = ftell($this->fp);
+
+            $value = unpack($this->types[$this->endian][$this->labels['float'][$sizeInBits]], $data);
+            return $value;
+        }
+    }
+
+    public function readString($sizeInBytes = 1) {
+        $data = fread($this->fp, $sizeInBytes);
+        if ($data !== false)
+            $this->offset += $bytes;
+        else
+            $this->offset = ftell($this->fp);
+        return $data;
+    }
+
+    public function readGroup($nameOrFieldsList) {
+        if (is_string($nameOrFieldsList)) {
+            if (isset($this->groups[$nameOrFieldsList]))
+                $fields = $this->groups[$nameOrFieldsList];
+            else
+                return null;
+        } else if (is_array($nameOrFieldsList)) {
+            $fields = $nameOrFieldsList;
+        }
+
+        $group = array();
+        $size = 0;
+        foreach ($fields as $field_name => $field_size_in_bits) {
+            if (strpos($field_name, ':') !== false) {
+                $field_name = substr($field_name, strpos($field_name, ':'));
+                switch (strstr($field_name, ':', true)) {
+                    case 's': $field_type = 'string'; break;
+                    case 'i': $field_type = 'integer'; break;
+                    case 'f': $field_type = 'float'; break;
+                }
+            } else
+                $field_type = 'bit';
+
+            if ($field_type == 'string')
+                $size += $field_size_in_bits;
+            else
+                $size += $field_size_in_bits / 8;
+        }
+
+        $cache = array();
+        for ($offset = 0; $offset < $size; $offset++) {
+            $cache[$i] = $this->readChar();
+        }
+        $bitOffset = 0;
+
+        foreach ($fields as $field_name => $field_size_in_bits) {
+            if (strpos($field_name, ':') !== false) {
+                $field_name = substr($field_name, strpos($field_name, ':'));
+                switch (strstr($field_name, ':', true)) {
+                    case 's': $field_type = 'string'; break;
+                    case 'i': $field_type = 'integer'; break;
+                    case 'f': $field_type = 'float'; break;
+                }
+            } else
+                $field_type = 'bit';
+
+            switch ($field_type) {
+                case 'bit':
+                    if ($bitOffset == 8) {
+                        $bitOffset = 0;
+                        $offset++;
+                    }
+
+                    $bitOffset++;
+                    $group[$field_name] = (bool) ($cache[$offset] >> $bitOffset);
+                    break;
+
+                case 's':
+                    if ($bitOffset != 0) {
+                        $bitOffset = 0;
+                        $offset++;
+                    }
+
+                    $group[$field_name] = null;
+                    for ($i = 0; $i < $field_size_in_bits; $i++)
+                        $group[$field_name] .= $cache[$offset];
+                        $offset++;
+                    break;
+
+                case 'i':
+                    if ($field_size_in_bits >= 16 && $field_size_in_bits <= 64 && $field_size_in_bits % 8 == 0) {
+                        $bytes = $field_size_in_bits / 8;
+                        $data = null;
+                        for ($i = 0; $i < $bytes; $i++) {
+                            $data .= $cache[$offset];
+                            $offset++;
+                        }
+
+                        $group[$field_name] = unpack($this->types[$this->endian][$this->labels['integer'][$field_size_in_bits]], $data);
+                    }
+                    break;
+
+                case 'f':
+                    if ($field_size_in_bits == 32 || $field_size_in_bits == 64) {
+                        $bytes = $field_size_in_bits / 8;
+                        $data = null;
+                        for ($i = 0; $i < $bytes; $i++) {
+                            $data .= $cache[$offset];
+                            $offset++;
+                        }
+
+                        $group[$field_name] = unpack($this->types[$this->endian][$this->labels['float'][$field_size_in_bits]], $data);
+                    }
+                    break;
+            }
+        }
+        return $group;
+    }
+
+    public function mark($name) {
+        $this->marks[$name] = $this->offset;
+    }
+
+    public function markOffset($offset, $name) {
+        $this->marks[$name] = $offset;
+    }
+
+    public function goto($offsetOrMark) {
+        if (is_string($offsetOrMark)) {
+            if (isset($this->marks[$offsetOrMark]))
+                fseek($this->fp, $this->marks[$offsetOrMark]);
+        } else {
+            fseek($this->fp, $offsetOrMark)
+        }
+    }
+
+    public function isMarked($name) {
+        return isset($this->marks[$name]);
+    }
+
+    public function skip($bytes) {
+        if (fseek($this->fp, $bytes, SEEK_CUR))
+            $this->offset += $bytes;
+        else
+            $this->offset = ftell($this->fp);
+    }
+
+    public function setEndian($endian) {
+        if (in_array($endian, array(self::BIG, self::LITTLE)))
+            $this->endian = $endian;
+    }
+
+    public function loadConfiguration($file) {
+        $config = parse_ini_file($file, true);
+        if (isset($config['main']['endian']))
+            $this->setEndian($config['main']['endian']);
+        foreach ($config as $section_name => $section_directives) {
+            if (strpos($section_name, 'group:') === 0) {
+                $group_name = substr($section_name, strlen('group:'));
+                $this->saveGroup($group_name, $section_directives);
+            }
+        }
+    }
+
+    public function saveConfiguration($file) {
+        $config = fopen($file, 'w');
+        fwrite($config, '[default]'.PHP_EOL.'endian='.$this->endian.PHP_EOL);
+        foreach ($this->groups as $group_name => $group_fields) {
+            fwrite($config, '[group:'.$group_name.']'.PHP_EOL);
+            foreach ($group_fields as $field_name => $field_size)
+                fwrite($config, $field_name.'='.$field_size.PHP_EOL);
+            fwrite($config, PHP_EOL);
+        }
+    }
+
+    public function saveGroup($name, $fields) {
+        $this->groups[$name] = $fields;
+    }
+
+    protected function readChar() {
+        $char = fgetc($this->fp);
+        if ($char !== false)
+            $this->offset++;
+        return $char;
+    }
+}
